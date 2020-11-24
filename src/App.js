e21dd2a72eb1fc9a483007dd8ae40d6b6623b589
Yashvirani <viraniyash2001@gmail.com>
2020-11-24 09:28:46 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import { MenuItem,FormControl,Select } from '@material-ui/core';
 import { useState,useEffect } from 'react';
+import InfoBox from './components/InfoBox/InfoBox';
+import Map from './components/Map/Map';
 
 function App() {
   const [countries,setCountries]= useState([]);
+  const [country,setCountry] = useState('Worldwide');
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -20,13 +23,18 @@ function App() {
           });
     }
     getCountriesData();
-  },[countries])
+  },[])
+
+  const onCountryChange = async (e) => {
+      const countryCode = e.target.value;
+      setCountry(countryCode);
+  }
   return (
     <div className="app">
       <div className="app__header">
         <h1>Covid-19 Tracker</h1>
         <FormControl className="app__dropdown">
-          <Select varient="outlined" value="abc">
+          <Select varient="outlined" onChange={onCountryChange} value={country}>
             <MenuItem value="worldwide">Worldwide</MenuItem>
             {
               countries.map(country => (
@@ -36,6 +44,14 @@ function App() {
           </Select>
         </FormControl>
       </div>
+
+      <div className="app__stats">
+        <InfoBox title="Coronavirus Cases" cases={2000} total={3000} />
+        <InfoBox title="Recoveries" cases={2000} total={3000}/>
+        <InfoBox title="Deaths" cases={2000} total={3000}/>
+      </div>
+
+      <Map />
      
     </div>
   );
